9f89e1dfd9e04ccf6a1bd12abe60f4c43a08cdf1
mohanshetty <65666015+mohanshetty@users.noreply.github.com>
2022-12-30 21:15:33 +0530

    Delete image-optimizer directory

--- image-optimizer/init.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-/* Load all the related plugin files */
-require_once \Altum\Plugin::get('image-optimizer')->path . 'ImageOptimizer.php';
-require_once \Altum\Plugin::get('image-optimizer')->path . 'Optimizer.php';
