3b3e2e19ad2b8552bec752731db9ae05b65c56eb
mohanshetty <65666015+mohanshetty@users.noreply.github.com>
2022-12-30 20:53:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 
+// let ctitle = document.getElementById("card-title-id")
+// ctitle.style.color = "red"
+
+// =======================
+
+// let ctitle = document.getElementsByClassName("card-title")
+/*
+let ctitle = document.querySelectorAll(".card-title")
+
+for (newcTitle of ctitle) {
+  newcTitle.style.color = "red"
+}
+
+let ctitles = document.getElementsByTagName("a")
+
+console.log(ctitles)
+
+
+let ctitles = document.getElementsByName("go-somewhere")[0]
+
+// console.log(ctitles)
+
+ctitles.style.color = "red"
+*/
+
+// =================
+// If this not working use console.log and check whether it returns HTML Collections or Single Objects, If not working use index and check this will works
+// =================
+
+let ctitles = document.querySelector(".card-body").getElementsByTagName("p")[0]
+console.log(ctitles)
+ctitles.style.color = "red"
+
+// for (newcTitle of ctitle) {
+//   newcTitle.style.color = "red"
+// }
